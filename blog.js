eecfbf53862eb9056b8454c3bd8d7bee0143ef2a
Joe Hitchens <joe@sleepless.com>
2016-02-15 00:50:47 -0500

    -

--- blog.js
+++ blog.js
@@ -72,6 +72,170 @@ var send = require('send');
 
 
 GET = function(req, res, qry) {
+	send(req, qry.pathname, {root: 'docroot'})
+	.on("error", function(e) {
+		build3(req, res, qry);
+	})
+	.pipe(res);
+}
+
+build3 = function(req, res, qry) {
+
+	var path = qry.pathname;				// raw path from GET line
+	if(path.includes("..")) {
+		r404(res, "naughty path: "+path);	// naughty path
+		return;
+	}
+
+	//path = path.substr(1);					// remove leading slash
+	path = path.replace( /\/+$/, "" ); 		// remove trailing slashes
+	if(path == "") {
+		path = "/home";
+	}
+	I("path="+path);
+
+	fs.readFile("./docroot"+path+".txt", "utf8", function(err, text) {
+
+		if(err) {
+			r404(res, "no txt file");
+			return;
+		}
+		I("loaded: ./docroot"+path+".txt");
+
+
+		var lines = text.split( /\n/ );
+		data = {};
+		for(var i = 0; i < lines.length; i++) {
+			var line = lines.shift().trim();
+			if(line == "") {
+				break;
+			}
+			var m = line.match( /^([-A-Za-z0-9]+): (.*)$/ );
+			if(!m) {
+				lines.unshift(line);
+				break;
+			}
+			data[m[1]] = m[2];
+		}
+		data["content"] = lines.join("\n");
+
+
+		var dirs = path.split("/");
+		I("dirs="+o2j(dirs));
+
+		var paths = [];
+		dirs.forEach(function(dir, i) {
+			paths[i] = (i == 0) ? dir : paths[i-1]+"/"+dir;
+		});
+		I("paths="+o2j(paths));
+
+		var shells = [];
+		var meet = new Meet();
+		paths.forEach(function(path, i) {
+			meet.start(function(done) {
+				var p = "./docroot"+path+"/shell.html";
+				I("trying "+p);
+				fs.readFile(p, "utf8", function(err, shell) {
+					if(err) {
+						shell = "{{content}}";
+					}
+					else {
+						I("shell loaded from "+p);
+					}
+					shells[i] = shell;
+					done();
+				});
+			});
+		});
+
+		meet.allDone(function() {
+			I("all done");
+
+			var html = "{{content}}";
+			shells.forEach(function(shell, i) {
+				html = html.replace( /{{content}}/, shell );
+			});
+
+			for(var k in data) {
+				var re = new RegExp( "{{"+k+"}}", "ig" );
+				html = html.replace( re, data[k] );
+			}
+
+			res.writeHead(200, {"ContentType": "text/html"});
+			res.write(html);
+			res.end();
+		});
+
+	});
+}
+
+build = function(req, res, qry) {
+
+	var path = qry.pathname;				// raw path from GET line
+	if(path.includes("..")) {
+		r404(res, "naughty path: "+path);	// naughty path
+		return;
+	}
+
+	path = path.substr(1);					// remove leading slash
+	path = path.replace( /\/+$/, "" ); 		// remove trailing slashes
+	if(path == "") {
+		path = "home";
+	}
+
+	var shell = null;
+	var body = null;
+
+	var m = new Meet();
+	m.start(function(done) {
+		fs.readFile("./docroot/shell.html", "utf8", function(err, data) {
+			if(!err) {
+				shell = data; 
+			}
+			done();
+		});
+	});
+	m.start(function(done) {
+		fs.readFile("./docroot/"+path+".txt", "utf8", function(err, data) {
+			if(!err) {
+				var lines = data.split( /\n/ );
+				body = {};
+				for(var i = 0; i < lines.length; i++) {
+					var line = lines.shift().trim();
+					if(line == "") {
+						break;
+					}
+					var m = line.match( /^([-A-Za-z0-9]+): (.*)$/ );
+					if(!m) {
+						lines.unshift(line);
+						break;
+					}
+					body[m[1]] = m[2];
+				}
+				body["body"] = lines.join("\n");
+			}
+			done();
+		});
+	});
+	m.allDone(function() {
+		if(shell && body) {
+			var html = ""+shell;
+			for(var k in body) {
+				var re = new RegExp( "{{"+k+"}}", "ig" );
+				html = html.replace( re, body[k] );
+			}
+			res.writeHead(200, {"ContentType": "text/html"});
+			res.write(html);
+			res.end();
+		}
+		else {
+			r404(res, "Not found");
+		}
+	});
+}
+
+
+oldbuild = function(req, res, qry) {
 
 	var path = qry.pathname;				// raw path from GET line
 	if(path.includes("..")) {
